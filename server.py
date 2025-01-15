9a96d607ee505a57658ce6f655cca7eb7e938a4d
jabouchleih <4330842+jabouchleih@users.noreply.github.com>
2025-01-15 21:30:22 +0100

    Create server.py - Add basic webserver that handles toggling HDMI and LCD Backlight

---
+++ server.py
@@ -0,0 +1,55 @@
+# server.py
+from flask import Flask, request, jsonify
+import subprocess
+import copy
+
+app = Flask(__name__)
+
+GPIO_PIN = "23"
+
+commands = {
+    "get_hdmi_state": ["vcgencmd", "display_power"],
+    "set_hdmi_state": ["vcgencmd", "display_power"], # either 1 (on) or 0 (off)
+    "get_backlight_state": ["raspi-gpio", "get", GPIO_PIN],
+    "set_backlight_state": ["raspi-gpio", "set", GPIO_PIN, "op"], #either dh (high, on) or dl (low, off)
+}
+
+@app.get("/state/backlight")
+def get_backlight_state():
+    command = commands.get("get_backlight_state")
+    print ("Executing command: ", command)
+    return subprocess.check_output(command)
+
+@app.get("/state/hdmi")
+def get_hdmi_state():
+    command = commands.get("get_hdmi_state")
+    print ("Executing command: ", command)
+    return subprocess.check_output(command)
+
+@app.post("/state/backlight/off")
+def turn_off_backlight():
+    command = copy.deepcopy(commands.get("set_backlight_state"))
+    command.append("dl")
+    print ("Executing command: ", command)
+    return subprocess.check_output(command)
+
+@app.post("/state/backlight/on")
+def turn_on_backlight():
+    command = copy.deepcopy(commands.get("set_backlight_state"))
+    command.append("dh")
+    print ("Executing command: ", command)
+    return subprocess.check_output(command)
+
+@app.post("/state/hdmi/off")
+def turn_off_hdmi():
+    command = copy.deepcopy(commands.get("set_hdmi_state"))
+    command.append("0")
+    print ("Executing command: ", command)
+    return subprocess.check_output(command)
+
+@app.post("/state/hdmi/on")
+def turn_on_hdmi():
+    command = copy.deepcopy(commands.get("set_hdmi_state"))
+    command.append("1")
+    print ("Executing command: ", command)
+    return subprocess.check_output(command)
